7d784f438c876920d1c2cbcbc3c10c995ee43458
Bastien <bastien.crettenand@hotmail.com>
2019-08-20 19:36:58 +0200

    Clear project

--- TestAPI/TestAPI/Models/Report.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestAPI.Models
-{
-    public class Report
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string PersonId { get; set; }
-    }
-}
